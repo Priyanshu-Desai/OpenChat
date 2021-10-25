be3716372ecabfb3d4432618238a24434c990ca9
Priyanshu-Desai <desai09p@gmail.com>
2021-10-25 17:21:11 +0100

    made web app consider newline in post - ver 1.1 release

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,7 @@ function Post(props){
     <div className='post'>
       <small className='post-text post-author'>by {props.name}</small>
       <h3 className='post-text post-heading'>{props.title}</h3>
-      <p className='post-text post-content'>{props.content}</p>
+      <pre className='post-text post-content'>{props.content}</pre>
     </div>
   )
 }
